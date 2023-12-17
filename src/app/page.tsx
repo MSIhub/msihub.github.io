7d5a24d87d93ebe515fae182606623ed4f9557a5
Mohamed Sadiq Ikbal <sadiq.ikbal@sttech.de>
2023-12-17 16:29:23 +0100

    fresh layouts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,24 +3,8 @@ import Footer from '@/components/layout/Footer';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      {/* Main Content */}
-      <div className="text-center">
-        <h1 className="text-4xl font-bold">Mohamed Sadiq IKBAL, PhD</h1>
-        <p className="mt-4 text-xl">Robotics Software Engineer</p>
-
-        {/* Image with rounded edges */}
-        <div className="inline-block overflow-hidden rounded-full"> {/* Rounded edges */}
-          <Image
-            src="/images/protfolioPic.JPG" // Adjust the path as needed
-            alt="Profile Image"
-            width={250}  // Adjust size as needed
-            height={250} // Adjust size as needed
-            className="object-cover"
-          />
-        </div>
-      </div>
-      <Footer />
+    <main className="">
+      
     </main>
   );
 }
